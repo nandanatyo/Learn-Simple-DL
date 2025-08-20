8cc5759b18d1d398a726e57cfdd75f1aea84409e
Kadek Nandana Tyo Nayotama <129768896+nandanatyo@users.noreply.github.com>
2025-08-20 08:32:12 +0700

    Initial commit

---
+++ diabetes.py
@@ -0,0 +1,30 @@
+from numpy import loadtxt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Load data
+data = loadtxt('diabetes.csv', delimiter=',')
+X, y = data[:, :-1], data[:, -1]
+
+# Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome
+
+# Definisikan model
+model = Sequential([
+    Dense(12, input_shape=(X.shape[1],), activation='relu'),
+    Dense(8, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+# Kompilasi model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Latih model
+model.fit(X, y, epochs=150, batch_size=10, verbose=1)
+
+# Evaluasi
+loss, accuracy = model.evaluate(X, y)
+print(f'Loss: {loss:.4f}, Akurasi: {accuracy:.4f}')
+
+# Prediksi
+prob = model.predict(X[0].reshape(1, -1))
+print("Probabilitas:", prob, "— Prediksi:", 1 if prob > 0.5 else 0)
